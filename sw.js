8aa5bb44c5f38dfd3f07431257f16fa3c89538a5
Taiki Hayashi <grandcolline@gmail.com>
2021-11-22 01:14:16 +0900

    cache update

--- sw.js
+++ sw.js
@@ -1,10 +1,14 @@
-let version = '0.18';
-var CACHE_NAME = 'grandcolline-cache-v90';
+let version = '0.19';
+var CACHE_NAME = 'grandcolline-cache-v91';
 
 var urlsToCache = [
   '/',
   '/manifest.json',
+  '/img/icons/grandcolline_128.png',
+  '/img/icons/grandcolline_152.png',
+  '/img/icons/grandcolline_144.png',
   '/img/icons/grandcolline_192.png',
+  '/img/icons/grandcolline_512.png',
   '/img/icons/grandcolline_maskable_192.png',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.css'
 ];
