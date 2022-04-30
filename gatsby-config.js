f765990ff030ecdbbc4fa60b5d2cebf5949d3232
Taiki Hayashi <grandcolline@gmail.com>
2022-04-30 21:12:30 +0900

    remove package

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,15 +7,7 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-mdx`,
-    // {
-    //   resolve: `gatsby-source-filesystem`,
-    //   options: {
-    //     name: `markdown`,
-    //     path: `${__dirname}/src/markdowns`,
-    //   },
-    // },
     `gatsby-plugin-postcss`,
-    // `gatsby-transformer-remark`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -30,56 +22,5 @@ module.exports = {
       },
     },
     `gatsby-plugin-offline`,
-    // {
-    //   resolve: `gatsby-plugin-feed`,
-    //   options: {
-    //     query: `
-    //       {
-    //         site {
-    //           siteMetadata {
-    //             title
-    //             description
-    //             siteUrl
-    //             site_url: siteUrl
-    //           }
-    //         }
-    //       }
-    //     `,
-    //     feeds: [
-    //       {
-    //         serialize: ({ query: { site, allMarkdownRemark } }) => {
-    //           return allMarkdownRemark.nodes.map(node => {
-    //             return Object.assign({}, node, {
-    //               title: node.frontmatter.title,
-    //               description: node.frontmatter.description,
-    //               date: node.frontmatter.date,
-    //               url: site.siteMetadata.siteUrl + node.frontmatter.slug,
-    //               guid: site.siteMetadata.siteUrl + node.frontmatter.slug,
-    //             })
-    //           })
-    //         },
-    //         query: `
-    //           {
-    //             allMarkdownRemark(
-    //               sort: { order: DESC, fields: [frontmatter___date] },
-    //               filter: { frontmatter: { slug: { regex: "/^/blog.*/" } } },
-    //             ) {
-    //               nodes {
-    //                 frontmatter {
-    //                   title
-    //                   slug
-    //                   description
-    //                   date
-    //                 }
-    //               }
-    //             }
-    //           }
-    //         `,
-    //         output: "/blog/rss.xml",
-    //         title: "grandcolline Blog",
-    //       },
-    //     ],
-    //   },
-    // },
   ],
 }
